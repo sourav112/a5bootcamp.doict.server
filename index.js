5d63894e39305928ea9ac188c92ef3f28345052a
souravadmin <engg.sourav112@gmail.com>
2024-10-23 21:42:33 +0600

    Demo Done

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json())
 
-const uri="mongodb://localhost:27017";
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqfai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//const uri="mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqfai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 //console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
